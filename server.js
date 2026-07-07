8fad5cb9c1d355ef92fa3d8d7e2def392323e8dc
HaneefaNatheer <natheerhaneefa4@gmail.com>
2026-07-07 15:33:31 +0530

    Added week 02 activities

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mockData = require('./data.js');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (request, response) => {
+    response.send(`Hello ! My Node.js server is running`);
+});
+
+app.get('/api/users', (request, response) => {
+    response.json(mockData);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});

4d36a111917fd69a1f7fd3dc528edb04742d09fc
HaneefaNatheer <natheerhaneefa4@gmail.com>
2026-07-17 15:31:00 +0530

    Completed Week 4 JWT Authentication

---
+++ week_04/Data/users.js
@@ -0,0 +1,14 @@
+const users = [
+  {
+    id: 1,
+    username: "Natheer",
+    password: "123456"
+  },
+  {
+    id: 2,
+    username: "Atheeka",
+    password: "password"
+  }
+];
+
+module.exports = users;

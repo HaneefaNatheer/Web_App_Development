9efc4bbcbc1ce19de8e976d287ff838cf9b1d431
HaneefaNatheer <natheerhaneefa4@gmail.com>
2026-07-24 10:22:30 +0530

    Add database changes

---
+++ week_05/Data/users.js
@@ -0,0 +1,14 @@
+const users = [
+  {
+    id: 1,
+    username: "Natheer",
+    password: "MyPwd@123",
+  },
+  {
+    id: 2,
+    username: "Atheeka",
+    password: "Pwd@123@",
+  },
+];
+
+module.exports = users;
